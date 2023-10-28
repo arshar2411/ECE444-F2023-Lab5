d34fbc1ca8b66d88564c3bdccdf5888709009ef2
arshar2411 <arjunmsharm@gmail.com>
2023-10-28 19:27:56 -0400

    replay complete

--- tests/app_test.py
+++ tests/app_test.py
@@ -74,9 +74,14 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
-    
+
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
